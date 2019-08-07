35127dc9b024f16f3b3a5229aafe24cf1473590a
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-08-07 22:01:35 +0900

    Stubs for settings controller

---
+++ MarkdownTodo/Views/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  MarkdownTodo
+//
+//  Created by Paul Traylor on 2019/08/07.
+//  Copyright © 2019 Paul Traylor. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+}
