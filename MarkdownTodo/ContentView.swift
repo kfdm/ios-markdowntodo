58a0a6faff1025d68deb40758a368dded14bdba0
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-07-11 18:19:53 +0900

    Initial Commit

---
+++ MarkdownTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownTodo
+//
+//  Created by Paul Traylor on 2020/07/11.
+//  Copyright © 2020 Paul Traylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
