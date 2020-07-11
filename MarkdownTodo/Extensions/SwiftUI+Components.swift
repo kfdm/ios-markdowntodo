5e050ce2f5e5b5155546b6768c4021aa92a7e5f8
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-07-11 22:22:40 +0900

    Move some of our smaller, helper components to its own file

---
+++ MarkdownTodo/Extensions/SwiftUI+Components.swift
@@ -0,0 +1,61 @@
+//
+//  SwiftUI+Components.swift
+//  MarkdownTodo
+//
+//  Created by Paul Traylor on 2020/07/11.
+//  Copyright © 2020 Paul Traylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct NameValue: View {
+    var label: String
+    var value: String
+
+    var body: some View {
+        HStack {
+            Text(label)
+            Spacer()
+            Text(value)
+        }
+    }
+}
+
+struct DateView: View {
+    var formatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+    var date: Date
+    var body: some View {
+        Text(formatter.string(from: date))
+    }
+}
+
+struct DateLabel: View {
+    var label: String
+    var date: Date
+
+    var body: some View {
+        HStack {
+            Text(label)
+            Spacer()
+            DateView(date: date)
+        }
+    }
+}
+
+extension DateLabel {
+    init(label: String, date: DateComponents) {
+        self.label = label
+        self.date = date.date!
+    }
+}
+
+extension DateView {
+    init(date from: DateComponents) {
+        date = from.date!
+    }
+}
--- MarkdownTodo/Views/ReminderDetail.swift
+++ MarkdownTodo/Views/ReminderDetail.swift
@@ -9,58 +9,6 @@
 import EventKit
 import SwiftUI
 
-struct NameValue: View {
-    var label: String
-    var value: String
-
-    var body: some View {
-        HStack {
-            Text(label)
-            Spacer()
-            Text(value)
-        }
-    }
-}
-
-struct DateView: View {
-    var formatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .short
-        return formatter
-    }
-    var date: Date
-    var body: some View {
-        Text(formatter.string(from: date))
-    }
-}
-
-struct DateLabel: View {
-    var label: String
-    var date: Date
-
-    var body: some View {
-        HStack {
-            Text(label)
-            Spacer()
-            DateView(date: date)
-        }
-    }
-}
-
-extension DateLabel {
-    init(label: String, date: DateComponents) {
-        self.label = label
-        self.date = date.date!
-    }
-}
-
-extension DateView {
-    init(date from: DateComponents) {
-        date = from.date!
-    }
-}
-
 struct ReminderDetail: View {
     var reminder: EKReminder
 
