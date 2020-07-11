f106a8e0bff0d9b86b6c6bf26716fada46edf663
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-07-11 18:37:38 +0900

    Tab test

--- MarkdownTodo/ContentView.swift
+++ MarkdownTodo/ContentView.swift
@@ -8,9 +8,30 @@
 
 import SwiftUI
 
+enum HomeTabs {
+    case calendar
+    case list
+    case settings
+}
+
 struct ContentView: View {
+    @State var selection = HomeTabs.calendar
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView(selection: $selection) {
+            Text("Tab Content 1").tabItem {
+                Image(systemName: "calendar")
+                Text("Planner")
+            }.tag(HomeTabs.calendar)
+            Text("Tab Content 2").tabItem {
+                Image(systemName: "list.bullet")
+                Text("List")
+            }.tag(HomeTabs.list)
+            Text("Tab Content 3").tabItem {
+                Image(systemName: "gear")
+                Text("Settings")
+            }.tag(HomeTabs.settings)
+        }
     }
 }
 
