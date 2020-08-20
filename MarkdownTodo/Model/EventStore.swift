319e594005031069367822517a4037ad5397aeba
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-08-20 17:59:45 +0900

    Some refactoring of EventStore for documentation

--- MarkdownTodo/Model/EventStore.swift
+++ MarkdownTodo/Model/EventStore.swift
@@ -45,50 +45,6 @@ class EventStore: ObservableObject {
             .sorted { $0.title < $1.title }
     }
 
-    func publisher(for predicate: NSPredicate) -> ReminderQuery {
-        let publisher = PassthroughSubject<[EKReminder], Never>()
-        eventStore.fetchReminders(matching: predicate) { (reminders) in
-            publisher.send(reminders ?? [])
-        }
-        return publisher
-    }
-
-    func reminders(for calendar: EKCalendar) -> NSPredicate {
-        return eventStore.predicateForIncompleteReminders(
-            withDueDateStarting: nil, ending: nil, calendars: [calendar])
-    }
-
-    func overdueReminders() -> NSPredicate {
-        let end = Calendar.current.date(byAdding: .day, value: 1, to: Date().midnight)
-        return eventStore.predicateForIncompleteReminders(
-            withDueDateStarting: nil, ending: end, calendars: nil)
-    }
-
-    func completeReminders() -> NSPredicate {
-        let start = Date().midnight
-        let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
-        return eventStore.predicateForCompletedReminders(
-            withCompletionDateStarting: start, ending: end, calendars: nil)
-    }
-
-    func reminders(for interval: DateInterval) -> NSPredicate {
-        return eventStore.predicateForIncompleteReminders(
-            withDueDateStarting: interval.start, ending: interval.end, calendars: nil)
-    }
-
-    func upcomingReminders(days: Int, start: Date = Date().midnight) -> NSPredicate {
-        let end = Calendar.current.date(byAdding: .day, value: days, to: start)
-        return eventStore.predicateForIncompleteReminders(
-            withDueDateStarting: start, ending: end, calendars: nil)
-    }
-
-    func reminders(for date: Date) -> NSPredicate {
-        let start = date.midnight
-        let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
-        return eventStore.predicateForIncompleteReminders(
-            withDueDateStarting: start, ending: end, calendars: nil)
-    }
-
     func toggleComplete(_ reminder: EKReminder) throws {
         print("Toggle reminder \(reminder)")
         if reminder.isCompleted {
@@ -113,4 +69,59 @@ class EventStore: ObservableObject {
     }
 }
 
-typealias ReminderQuery = PassthroughSubject<[EKReminder], Never>
+// MARK:- Some lower level queries to convert NSPredicate to Publisher
+extension EventStore {
+    typealias EKReminderPublisher = PassthroughSubject<[EKReminder], Never>
+
+    func publisher(for predicate: NSPredicate) -> EKReminderPublisher {
+        let publisher = PassthroughSubject<[EKReminder], Never>()
+        eventStore.fetchReminders(matching: predicate) { (reminders) in
+            publisher.send(reminders ?? [])
+        }
+        return publisher
+    }
+
+    func reminders(for interval: DateInterval) -> NSPredicate {
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: interval.start, ending: interval.end, calendars: nil)
+    }
+
+    func reminders(for calendar: EKCalendar) -> NSPredicate {
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: nil, ending: nil, calendars: [calendar])
+    }
+
+    func reminders(for date: Date) -> NSPredicate {
+        let start = date.midnight
+        let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: start, ending: end, calendars: nil)
+    }
+}
+
+// MARK:- Some useful preselected queries
+extension EventStore {
+    func completeReminders(for date: Date = Date()) -> NSPredicate {
+        let start = date.midnight
+        let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
+        return eventStore.predicateForCompletedReminders(
+            withCompletionDateStarting: start, ending: end, calendars: nil)
+    }
+
+    func scheduledReminders() -> NSPredicate {
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: .distantPast, ending: .distantFuture, calendars: nil)
+    }
+
+    func upcomingReminders(days: Int, start: Date = Date().midnight) -> NSPredicate {
+        let end = Calendar.current.date(byAdding: .day, value: days, to: start)
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: start, ending: end, calendars: nil)
+    }
+
+    func overdueReminders() -> NSPredicate {
+        let end = Calendar.current.date(byAdding: .day, value: 1, to: Date().midnight)
+        return eventStore.predicateForIncompleteReminders(
+            withDueDateStarting: nil, ending: end, calendars: nil)
+    }
+}
